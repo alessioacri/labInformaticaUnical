68d3fa17320b146080968e8e9b81eabc4b6f8e10
alessioacri <71268855+alessioacri@users.noreply.github.com>
2026-06-07 20:16:31 +0200

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -1,1 +1,10 @@
+// biblioteca.h
+typedef struct Libro {
+    char titolo[100];
+    char autore[100];
+    int copie;
+    int anno;
+} Libro;
 
+// questo è un prototipo che dice al programma che esiste questa funzione
+void inserisciLibro(Libro nuovoLibro);
