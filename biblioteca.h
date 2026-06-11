52d2ec7077a914334867bd131f9820aa1ab2ab20
alessioacri <71268855+alessioacri@users.noreply.github.com>
2026-06-11 12:51:34 +0200

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -1,4 +1,12 @@
 // biblioteca.h
+// contiene le definizioni delle strutture dati e delle funzioni utilizzate
+
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+#define MAX_STR 100
+#define HASH_SIZE 10
+
 typedef struct Libro {
     char titolo[100];
     char autore[100];
