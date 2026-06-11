ce068f3fbf5ea8c71b92c21905f94874fa8d22ff
alessioacri <71268855+alessioacri@users.noreply.github.com>
2026-06-11 12:53:27 +0200

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -7,12 +7,28 @@
 #define MAX_STR 100
 #define HASH_SIZE 10
 
-typedef struct Libro {
-    char titolo[100];
-    char autore[100];
-    int copie;
+// L1b -> Enum per lo stato di disponibilità del libro
+typedef enum {
+    DISPONIBILE,
+    IN_PRESTITO,
+    ESAURITO
+} StatoLibro;
+
+// L5b -> Struct principale che rappresenta un Libro
+typedef struct {
+    char id[MAX_STR];        // Stringa univoca (es. ISBN o codice)
+    char titolo[MAX_STR];
+    char autore[MAX_STR];
     int anno;
+    int copie;
+    StatoLibro stato;
 } Libro;
+
+// L8b - Nodo della Lista Concatenata principale (Il catalogo)
+typedef struct NodoLista {
+    Libro info;
+    struct NodoLista* next;
+} NodoLista;
 
 // questo è un prototipo che dice al programma che esiste questa funzione
 void inserisciLibro(Libro nuovoLibro);
