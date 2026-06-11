c4c9348698822db1019944e82cf685ab13ff73df
alessioacri <71268855+alessioacri@users.noreply.github.com>
2026-06-11 14:28:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
+//main.c
+//Questo file contiene l'interfaccia utente e coordina i vari moduli del programma.
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "biblioteca.h"
 
 int main() {
     printf("Benvenuto nella gestione biblioteca!\n");
-    return 0;
-}
+    printf("Ecco le opzioni disponibili:\n");
+    NodoLista* catalogo = NULL;
+    NodoHash* tabellaHash[HASH_SIZE] = { NULL };
+    CodaAttesa codaPrestiti;
+    initCoda(&codaPrestiti);
+    NodoPila* pilaResi = NULL;
